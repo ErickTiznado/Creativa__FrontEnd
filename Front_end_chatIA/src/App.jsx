7444571e6d7641e8e87b6d20b6a92fb35145d48a
Eliseo Lozano <124175479+eliseolozanowp@users.noreply.github.com>
2026-01-16 02:13:21 +0000

    Pantalla Inicial de Diseñador.

--- Front_end_chatIA/src/App.jsx
+++ Front_end_chatIA/src/App.jsx
@@ -2,16 +2,30 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Navbar from './components/Navbar/Navbar.jsx'
 import ViewCampaignsMarketing from './layouts/ViewCampaignsMarketing/ViewCampaignsMarketing.jsx';
+import ViewAssignmentsDesigner from './layouts/ViewAssignmentsDesigner/ViewAssignmentsDesigner.jsx';
 import ChatPage from './pages/ChatPage';
 import './App.css'
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<ViewCampaignsMarketing />} />
+        <Route path="/" element={
+            <>
+              <Navbar role='Marketing' />
+              <ViewCampaignsMarketing />
+            </>
+          }/>
+
         <Route path="/chat" element={<ChatPage />} /> {/* Nueva vista del chatbox */}
+
+        <Route path="/designer" element={
+            <>
+              <Navbar role='Diseñador' />
+              <ViewAssignmentsDesigner />
+            </>
+          }/>
+
       </Routes>
     </Router>
   )
