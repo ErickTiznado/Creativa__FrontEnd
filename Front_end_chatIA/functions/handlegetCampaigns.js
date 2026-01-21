2b3d995753f646723ba83c870c79fe8bf6a6843c
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2026-01-21 11:13:24 -0600

    feat: Mejoras en el chat de marketing y persistencia de chats

---
+++ Front_end_chatIA/functions/handlegetCampaigns.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export let campaigns = [];
+
+export const handleGetCampaigns = async () => {
+    try {
+        const response = await axios.get('http://localhost:3000/campaigns')
+        campaigns = response.data
+        return response.data
+    } catch (e) {
+        console.log(e)
+    }
+}
--- Front_end_chatIA/src/components/Cards/Cards.jsx
+++ Front_end_chatIA/src/components/Cards/Cards.jsx
@@ -2,52 +2,39 @@ import './cards.css';
 import Reloj from '../../assets/img/reloj.png';
 import FotoUser from '../../assets/img/user.jpg';
 import { useState } from 'react';
+
+
 function Cards(props) {
     // const [color, setColor] = useState('var(--color-proceso)');
     var color = 'var(--color-proceso)';
-    const estado = 'Aprobado';
-    if(estado === 'Enviado') {
+    const estado = props.estado;
+    console.log(props)
+    if (estado === 'Enviado') {
         color = 'var(--color-proceso)';
-    } else if(estado === 'Cancelado') {
+    } else if (estado === 'Cancelado') {
         color = 'var(--color-cancelado)';
-    } else if(estado === 'Aprobado') {
+    } else if (estado === 'Aprobado') {
         color = 'var(--color-aprobado)';
-    } else if(estado === 'Rechazado') {
+    } else if (estado === 'Rechazado') {
         color = 'var(--color-rechazado)';
     }
+
     return (
         <div className="cards-container">
             <img className='imgCard' src={Reloj} alt="Reloj" />
             <div className='InfoCard'>
-                <h4>Reclutamientos de pasantes</h4>
+                <h4>{props.titulo}</h4>
                 <div className="progreso">
-                    <div className='Estado' style={{backgroundColor: color}}></div>
-                    Enviado
+                    <div className='Estado' style={{ backgroundColor: color }}></div>
+                    {props.estado}
                 </div>
                 <div className="fecha">
                     <img className='FotoUser' src={FotoUser} alt="Foto" />
-                    <p className='nameUser'>Katalina</p>
-                    <p className='date'>25 Dic</p>
+                    <p>{props.usuario}</p>
+                    <p>{props.fecha}</p>
                 </div>
             </div>
         </div>
     );
-    // return (
-    //     <div className="cards-container">
-    //         <img className='imgCard' src={Reloj} alt="Reloj" />
-    //         <div className='InfoCard'>
-    //             <h4>{props.titulo}</h4>
-    //             <div className="progreso">
-    //                 <div className='Estado' style={{backgroundColor: color}}></div>
-    //                 {props.estado}
-    //             </div>
-    //             <div className="fecha">
-    //                 <img className='FotoUser' src={FotoUser} alt="Foto" />
-    //                 <p>{props.usuario}</p>
-    //                 <p>{props.fecha}</p>
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
 }
 export default Cards;
